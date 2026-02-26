f7300713932edd2dea4f168237a863d4792071ab
Akshaygn404 <iakshaygn2004@gmail.com>
2026-02-26 11:29:32 +0530

    Initial Spring Boot setup with MySQL configuration

---
+++ src/main/java/com/akshay/dcrs/DcrsApplication.java
@@ -0,0 +1,13 @@
+package com.akshay.dcrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akshay/dcrs/DcrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshay.dcrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
